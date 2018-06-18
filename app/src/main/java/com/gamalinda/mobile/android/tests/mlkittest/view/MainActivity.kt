9154c8a4032f1ac4636678f42c183014f5a4ed03
Ino Gamalinda <ino@gamalinda.com>
2018-06-19 01:56:52 +0800

    Implement bodge of MLKit Text Vision API
    Signed-off-by: Ino Gamalinda <ino@gamalinda.com>

--- app/src/main/java/com/gamalinda/mobile/android/tests/mlkittest/view/MainActivity.kt
+++ app/src/main/java/com/gamalinda/mobile/android/tests/mlkittest/view/MainActivity.kt
@@ -10,7 +10,12 @@ import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import com.gamalinda.mobile.android.tests.mlkittest.R
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.text.FirebaseVisionText
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -23,6 +28,8 @@ class MainActivity : AppCompatActivity() {
         private const val REQUEST_IMAGE_CAPTURE = 1
     }
 
+    lateinit var prediction: TextView
+    lateinit var confidence: TextView
     lateinit var pictureView: ImageView
     lateinit var photoPath: String
 
@@ -31,6 +38,8 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_base_layout)
 
+        prediction = findViewById(R.id.prediction)
+        confidence = findViewById(R.id.confidence)
         pictureView = findViewById(R.id.pictureView)
         val button = findViewById<Button>(R.id.takePictureButton)
         button.setOnClickListener { takePicture() }
@@ -46,14 +55,6 @@ class MainActivity : AppCompatActivity() {
                     photoFile.canonicalFile)
             pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
             startActivityForResult(pictureIntent, REQUEST_IMAGE_CAPTURE)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            setPic()
         }
     }
 
@@ -74,7 +75,7 @@ class MainActivity : AppCompatActivity() {
         return image
     }
 
-    private fun setPic() {
+    private fun getPic(): Bitmap {
         // Get the dimensions of the View
         val targetW = pictureView.getWidth()
         val targetH = pictureView.getHeight()
@@ -96,5 +97,48 @@ class MainActivity : AppCompatActivity() {
 
         val bitmap = BitmapFactory.decodeFile(photoPath, bmOptions)
         pictureView.setImageBitmap(bitmap)
+
+        return bitmap
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val bitmap = getPic()
+            pictureView.setImageBitmap(bitmap)
+            doTensorFlowImageClassify(bitmap)
+        }
+    }
+
+    private fun doTensorFlowImageClassify(bitmapImage: Bitmap) {
+        val fvi = FirebaseVisionImage.fromBitmap(bitmapImage)
+        val detector: FirebaseVisionTextDetector = FirebaseVision.getInstance().visionTextDetector
+        val result = detector.detectInImage(fvi)
+                .addOnSuccessListener {
+                    // Task completed successfully
+                    // ...
+                    doFirebaseSuccess(it)
+                }
+                .addOnFailureListener {
+                    // Task failed with an exception
+                    // ...
+                }
+    }
+
+    private fun doFirebaseSuccess(fvt: FirebaseVisionText) {
+        for (block in fvt.getBlocks()) {
+            val boundingBox = block.getBoundingBox()
+            val cornerPoints = block.getCornerPoints()
+            val text = block.getText()
+
+            for (line in block.getLines()) {
+                // ...
+                for (element in line.getElements()) {
+                    // ...
+                }
+                prediction.text = line.text
+            }
+        }
     }
 }
