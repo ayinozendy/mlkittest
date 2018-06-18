9c52ee659c89bce72b37827ee12039d387234fbf
Ino Gamalinda <ino@gamalinda.com>
2018-06-19 01:36:35 +0800

    Implement picture taker
    Signed-off-by: Ino Gamalinda <ino@gamalinda.com>

--- app/src/main/java/com/gamalinda/mobile/android/tests/mlkittest/view/MainActivity.kt
+++ app/src/main/java/com/gamalinda/mobile/android/tests/mlkittest/view/MainActivity.kt
@@ -1,13 +1,100 @@
 package com.gamalinda.mobile.android.tests.mlkittest.view
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
 import com.gamalinda.mobile.android.tests.mlkittest.R
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+    }
+
+    lateinit var pictureView: ImageView
+    lateinit var photoPath: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_base_layout)
+
+        pictureView = findViewById(R.id.pictureView)
+        val button = findViewById<Button>(R.id.takePictureButton)
+        button.setOnClickListener { takePicture() }
+    }
+
+    private fun takePicture() {
+        val pictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        pictureIntent.resolveActivity(packageManager)?.let {
+            val photoFile = createImageFile()
+            photoFile.absolutePath
+            val photoURI = FileProvider.getUriForFile(this,
+                    "com.gamalinda.mobile.android.tests.mlkittest.fileprovider",
+                    photoFile.canonicalFile)
+            pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            startActivityForResult(pictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            setPic()
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = filesDir
+        val image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir      /* directory */
+        )
+
+        // Save a file: path for use with ACTION_VIEW intents
+        photoPath = image.absolutePath
+        return image
+    }
+
+    private fun setPic() {
+        // Get the dimensions of the View
+        val targetW = pictureView.getWidth()
+        val targetH = pictureView.getHeight()
+
+        // Get the dimensions of the bitmap
+        val bmOptions = BitmapFactory.Options()
+        bmOptions.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(photoPath, bmOptions)
+        val photoW = bmOptions.outWidth
+        val photoH = bmOptions.outHeight
+
+        // Determine how much to scale down the image
+        val scaleFactor = Math.min(photoW / targetW, photoH / targetH)
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false
+        bmOptions.inSampleSize = scaleFactor
+        bmOptions.inPurgeable = true
+
+        val bitmap = BitmapFactory.decodeFile(photoPath, bmOptions)
+        pictureView.setImageBitmap(bitmap)
     }
 }
